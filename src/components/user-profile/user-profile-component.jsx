8f58bf02f22a05806a0045519c19c9b6da2298d8
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-03-15 21:58:32 -0600

    worked on user profile page

--- src/components/user-profile/user-profile-component.jsx
+++ src/components/user-profile/user-profile-component.jsx
@@ -1,13 +1,47 @@
 import React, { useState } from 'react';
-
-import './user-profile-page-style.scss';
+import { Avatar, Button, Paper, Typography } from '@material-ui/core';
+import './user-profile-style.scss';
 
 const UserProfile = () => {
-
+    const [ user, setUser ] = useState({
+        publicName: 'Ariel Lin',
+        contributionAmount: 2,
+        location: 'Calgary, Canada',
+        joined: 'March 2021'
+    });
+    const { publicName, contributionAmount, location, joined } = user;
 
     return (
         <div className='user-profile-page'>
-            
+            <Paper className='profile-header'>
+                <div className='profile-header-1'>
+                    <Avatar className='user-avatar'>N</Avatar>
+                    <div className='user-info'>
+                        <Typography variant='h6' gutterBottom >{publicName}</Typography>
+                        <Typography variant='subtitle1' >Contributions</Typography>
+                        <Typography variant='subtitle1' color='textSecondary' gutterBottom >{contributionAmount}</Typography>
+                    </div>
+                </div>
+                <div className='profile-header-2'>
+                    <Button className="update-button" variant="contained" color="primary">
+                        Update Profile
+                    </Button>
+                </div>
+            </Paper>
+            <Paper className='profile-body'>
+                <div className='user-detail'>
+                    <i className="fa fa-map-marker"></i>
+                    <Typography className="info" variant='subtitle1'>{location}</Typography>
+                </div>
+                <div className='user-detail'>
+                    <i class="fa fa-calendar"></i>
+                    <Typography className="info" variant='subtitle1'>{joined}</Typography>
+                </div>
+                <div className='user-detail'>
+                    <i class="fa fa-pencil"></i>
+                    <Button className="review-button" color="primary">Write review</Button>
+                </div>
+            </Paper>
         </div>
     )
 }
--- src/containers/admin-dashboard-page/admin-dashboard-page-component.jsx
+++ src/containers/admin-dashboard-page/admin-dashboard-page-component.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
-
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+import UserProfile from '../../components/user-profile/user-profile-component';
 import './admin-dashboard-page-style.scss';
 
 const AdminDashboardPage = () => {
-
+    const [ value, setValue ] = useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <div className='admin-dashboard-page'>
+            <div position="static" className='admin-dashboard-container'>
+                <Tabs 
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                    centered
+                >
+                    <Tab label="User Profile" className='admin-tab' />
+                    <Tab label="Account Info" className='admin-tab' />
+                    <Tab label="My Reviews" className='admin-tab' />
+                </Tabs>
+                <UserProfile />
+                {
+                    // value === 0 ? <SignIn /> : <SignUp />
+                }
+            </div>
             
         </div>
     )
