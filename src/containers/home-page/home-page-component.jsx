7503b0baf7d1a14571f85be44797b5f99e5bf153
ariellinfy <72408793+ariellinfy@users.noreply.github.com>
2021-05-11 15:19:45 -0600

    fixed language

--- src/containers/home-page/home-page-component.jsx
+++ src/containers/home-page/home-page-component.jsx
@@ -33,7 +33,7 @@ const HomePage = ({ resetCreateRestaurantStatus, resetUpdateRestaurantStatus, re
             <header className='homepage-header'>
                 <div className='homepage-welcome-container'>
                     <Typography variant="h1">Taste The Natural</Typography>
-                    <Typography variant="h6">Let us find out the romance in the world of vegetables! Come vegetarian with me!</Typography>
+                    <Typography variant="h6">Let's find out the romance in the world of vegetables! Come vegetarian with us!</Typography>
                 </div>
             </header>
             <div className='homepage-body'>
@@ -49,7 +49,7 @@ const HomePage = ({ resetCreateRestaurantStatus, resetUpdateRestaurantStatus, re
                 <div className='homepage-feature-container feature-container-2'>
                     <div className='feature-text'>
                         <Typography variant="h4">Discover restaurants</Typography>
-                        <Typography variant="subtitle1">Wondering where to eat? Discover more fabulous and delicious restaurants in Explore!</Typography>
+                        <Typography variant="subtitle1">Wondering where to eat? Discover more delicious and amazing restaurants in Explore!</Typography>
                     </div>
                     <div className='feature-image image-2'>
                         <img className='image-2' alt='homepage-img-2' src={discoverImage} />
@@ -61,7 +61,7 @@ const HomePage = ({ resetCreateRestaurantStatus, resetUpdateRestaurantStatus, re
                     </div>
                     <div className='feature-text'>
                         <Typography variant="h4">Add a place</Typography>
-                        <Typography variant="subtitle1">You know an awesome restaurant but it's not here? Add it on! so more people can benefit from the beauty of green diets!</Typography>
+                        <Typography variant="subtitle1">You know an awesome restaurant but it's not here? Add it on! More people will benefit from your sharing and sense the beauty of green diets!</Typography>
                     </div>
                 </div>
             </div>
